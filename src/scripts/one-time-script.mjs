b160f9ef255340c0449b01e29450063ba2fc629e
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-22 12:49:52 -0300

    feat(script): script for extracting video file frames (one-time-script)

---
+++ src/scripts/one-time-script.mjs
@@ -0,0 +1,14 @@
+import extractFrames from 'ffmpeg-extract-frames'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const videoPath = path.resolve(__dirname, '../assets/BadApple.mp4')
+const framesPath = path.resolve(__dirname, '../assets/frames')
+
+await extractFrames({
+  input: videoPath,
+  output: framesPath + '/frame-%d.jpeg'
+})
