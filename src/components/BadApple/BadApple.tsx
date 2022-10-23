69f6edb22e4388bf352131acb3f64ae1d0cf6941
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-23 11:55:45 -0300

    refactor: change function names and places, also deleting not needed consts

--- src/components/BadApple/BadApple.tsx
+++ src/components/BadApple/BadApple.tsx
@@ -7,21 +7,12 @@ interface Props {
 }
 
 const BadApple = ({ width, height, customStyles }: Props) => {
-  const asciiImage = useRef<HTMLPreElement>(null)
+  const playerComponent = useRef<HTMLPreElement>(null)
 
-  const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. '
-  const rampLength = grayRamp.length
+  const symbols = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. '
 
-  function toGrayScale (r: number, g: number, b: number) {
-    return Math.floor(0.2126 * r + 0.7152 * g + 0.0722 * b)
-  }
-
-  function getCharacterForGrayScale (grayScale: number) {
-    const index = Math.floor(grayScale / 255 * (rampLength - 1))
-    return grayRamp[index]
-  }
-
-  function convertToGrayScales (ctx: CanvasRenderingContext2D, imgWidth: number, imgHeight: number) {
+  const transformIntoGrayScale = (r: number, g: number, b: number) => Math.floor(0.2126 * r + 0.7152 * g + 0.0722 * b)
+  const transformIntoGrayScales = (ctx: CanvasRenderingContext2D, imgWidth: number, imgHeight: number) => {
     const imgData = ctx.getImageData(0, 0, imgWidth, imgHeight)
 
     const grayScales = []
@@ -31,7 +22,7 @@ const BadApple = ({ width, height, customStyles }: Props) => {
       const g: number = imgData.data[i + 1]
       const b: number = imgData.data[i + 2]
 
-      const grayScale = toGrayScale(r, g, b)
+      const grayScale = transformIntoGrayScale(r, g, b)
       imgData.data[i] = imgData.data[i + 1] = imgData.data[
         i + 2
       ] = grayScale
@@ -44,9 +35,14 @@ const BadApple = ({ width, height, customStyles }: Props) => {
     return grayScales
   }
 
-  function drawAscii (grayScales: number[], imgWidth: number) {
+  const getSymbolByGrayScale = (grayScale: number) => {
+    const index = Math.floor(grayScale / 255 * (symbols.length - 1))
+    return symbols[index]
+  }
+
+  const drawAsciiImage = (grayScales: number[], imgWidth: number) => {
     const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
-      let nextChars = getCharacterForGrayScale(grayScale)
+      let nextChars = getSymbolByGrayScale(grayScale)
 
       if ((index + 1) % imgWidth === 0) {
         nextChars += '\n'
@@ -55,12 +51,12 @@ const BadApple = ({ width, height, customStyles }: Props) => {
       return asciiImage + nextChars
     }, '')
 
-    if (asciiImage.current) {
-      asciiImage.current.textContent = ascii
+    if (playerComponent.current) {
+      playerComponent.current.textContent = ascii
     }
   }
 
-  function getFontRatio () {
+  const getFontRatio = () => {
     const pre = document.createElement('pre')
     pre.style.display = 'inline'
     pre.textContent = ' '
@@ -72,7 +68,7 @@ const BadApple = ({ width, height, customStyles }: Props) => {
     return height / width
   }
 
-  function clampDimensions (imgWidth: number, imgHeight: number) {
+  const clampDimensions = (imgWidth: number, imgHeight: number) => {
     const rectifiedWidth = Math.floor(getFontRatio() * imgWidth)
 
     if (imgHeight > height) {
@@ -98,7 +94,7 @@ const BadApple = ({ width, height, customStyles }: Props) => {
 
     const render = async () => {
       for (let i = 1; i <= frames; i++) {
-        if (context && asciiImage) {
+        if (context && playerComponent.current) {
           const img = await import(`../../assets/frames/frame-${i}.jpeg`)
           const image = new Image()
           image.src = img.default
@@ -111,9 +107,9 @@ const BadApple = ({ width, height, customStyles }: Props) => {
 
             context.drawImage(image, 0, 0, imgWidth, imgHeight)
 
-            const grayScales = convertToGrayScales(context, imgWidth, imgHeight)
+            const grayScales = transformIntoGrayScales(context, imgWidth, imgHeight)
 
-            drawAscii(grayScales, imgWidth)
+            drawAsciiImage(grayScales, imgWidth)
           }
         }
       }
@@ -126,7 +122,7 @@ const BadApple = ({ width, height, customStyles }: Props) => {
     <>
       <canvas id="canvas" width={width} height={height} style={{ display: 'none' }} />
       <pre
-        ref={asciiImage}
+        ref={playerComponent}
         style={{
           ...customStyles,
         }}
