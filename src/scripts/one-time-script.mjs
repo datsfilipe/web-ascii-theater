b78ed02f098bbaf38c197b1ed67b24e17f965350
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-27 12:11:42 -0300

    feat: set fix fps in extracting frames to 30

--- src/scripts/one-time-script.mjs
+++ src/scripts/one-time-script.mjs
@@ -6,9 +6,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const videoPath = path.resolve(__dirname, '../assets/BadApple.mp4')
-const framesPath = path.resolve(__dirname, '../assets/frames')
+const framesPath = path.resolve(__dirname, '../../public/frames')
 
 await extractFrames({
   input: videoPath,
-  output: framesPath + '/frame-%d.jpeg'
+  output: framesPath + '/frame-%d.jpeg',
+  fps: 30,
 })
