c3389528d9845b54c6a2a2324ee3a5062e1e3434
datsfilipe <datsfilipe.pro@proton.me>
2023-02-14 06:53:32 -0300

    refactor(scripts): add download script and use extract frames script name instead of one-time-script

---
+++ src/scripts/download-vid.mjs
@@ -0,0 +1,37 @@
+import youtubedl from 'youtube-dl-exec'
+import readline from 'readline'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const downloadDir = path.resolve(__dirname, '../../public')
+
+async function downloadVideo(url) {
+  try {
+    await youtubedl(url, {
+      noCheckCertificates: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+      addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+      format: 134,
+      output: path.join(downloadDir, 'video.%(ext)s'),
+    })
+
+    console.log(`Video downloaded to ${downloadDir}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+rl.question('Insert video URL from YouTube: ', async (answer) => {
+  if (answer) await downloadVideo(answer)
+
+  console.log('Closing interface...')
+  process.exit()
+})
--- src/scripts/extract-frames.mjs
+++ src/scripts/extract-frames.mjs
@@ -1,15 +1,19 @@
 import extractFrames from 'ffmpeg-extract-frames'
 import path from 'path'
+import fs from 'fs'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const videoPath = path.resolve(__dirname, '../assets/BadApple.mp4')
+const videoPath = path.resolve(__dirname, '../../public/video.mp4')
 const framesPath = path.resolve(__dirname, '../../public/frames')
+
+fs.rmdirSync(framesPath, { recursive: true })
+fs.mkdirSync(framesPath, { recursive: true })
 
 await extractFrames({
   input: videoPath,
   output: framesPath + '/frame-%d.jpeg',
-  fps: 30,
+  fps: 25,
 })
