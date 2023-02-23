39ce70d1aa78b1bd3e4d67fd22e3c9f03cabdfb1
datsfilipe <datsfilipe.pro@proton.me>
2023-02-23 16:57:14 -0300

    feat(extract-frames): trying to reduce image quality

--- src/scripts/extract-frames.mjs
+++ src/scripts/extract-frames.mjs
@@ -1,6 +1,7 @@
 import extractFrames from 'ffmpeg-extract-frames'
 import path from 'path'
 import fs from 'fs'
+import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -8,14 +9,30 @@ const __dirname = path.dirname(__filename)
 
 const videoPath = path.resolve(__dirname, '../../public/video.mp4')
 const framesPath = path.resolve(__dirname, '../../public/frames')
+const extractedFramesPath = path.resolve(__dirname, '../../public/frames/extracts')
 
-fs.rmdirSync(framesPath, { recursive: true })
+// fs.rmdirSync(framesPath, { recursive: true })
+
 fs.mkdirSync(framesPath, { recursive: true })
+fs.mkdirSync(extractedFramesPath, { recursive: true })
 
 await extractFrames({
   input: videoPath,
-  output: framesPath + '/frame-%d.jpeg',
+  output: extractedFramesPath + '/frame-%d.jpg',
   fps: 25,
 })
+
+const files = fs.readdirSync(extractedFramesPath)
+const promises = files.map((file) => {
+  const filePath = path.resolve(extractedFramesPath + '/' + file)
+  return sharp(filePath)
+    .resize(200)
+    .jpeg({ quality: 10 })
+    .toFile(framesPath + '/' + file)
+})
+
+await Promise.all(promises)
+
+fs.rmdirSync(extractedFramesPath, { recursive: true })
 
 export default extractFrames
