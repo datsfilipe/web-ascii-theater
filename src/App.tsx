44ccd4c1cbf88424b0529a726aea1f5179e56ae4
datsfilipe <datsfilipe.foss@proton.me>
2023-05-09 14:17:02 -0300

    refactor: update review config

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,37 @@ import React from 'react'
 import AsciiTheater from './components/AsciiTheater/AsciiTheater'
 import './App.css'
 
-    <div
-      className="App"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh'
-      }}
-    >
-      <AsciiTheater
-        width={80}
-        height={50}
-        framesDir={'/frames'}
-        framesCount={5478}
-        customStyles={{
-          fontSize: '9px',
-          width: 'fit-content',
-          height: 'fit-content',
-          overflow: 'auto',
-          margin: 0,
-          padding: 0,
-          backgroundColor: 'black',
-          color: 'white',
-          textAlign: 'center'
 export default function App() {
   return (
     <React.StrictMode>
+      <div
+        className="App"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
         }}
-        loop
-      />
-    </div>
+      >
+        <AsciiTheater
+          width={80}
+          height={50}
+          framesDir={'/frames'}
+          framesCount={5478}
+          customStyles={{
+            fontSize: '14px',
+            width: 'fit-content',
+            height: 'fit-content',
+            overflow: 'auto',
+            margin: 0,
+            padding: 0,
+            backgroundColor: 'black',
+            color: 'white',
+            textAlign: 'center'
+          }}
+          loop
+        />
+      </div>
     </React.StrictMode>
   )
 }
